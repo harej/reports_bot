4ba2d10e4aad69dbbe48d55f2b7253226e6e059c
harej <jamesmhare@gmail.com>
2015-05-11 15:52:30 -0400

    Undo

--- new_discussions.py
+++ new_discussions.py
@@ -20,7 +20,7 @@ def main():
     wikitime = now.strftime('%Y%m%d%H%M%S') # converts timestamp to MediaWiki format
     thirtyminutesago = (now - datetime.timedelta(minutes=30)).strftime('%Y%m%d%H%M%S')
     
-    query = wptools.query('wiki', 'select distinct recentchanges.rc_title, recentchanges.rc_comment, recentchances.rc_timestamp, page.page_namespace from recentchanges join page on recentchanges.rc_namespace = page.page_namespace and recentchanges.rc_title = page.page_title join categorylinks on page.page_id=categorylinks.cl_from where rc_timestamp >= %s and rc_timestamp < %s and rc_comment like "% new section" and rc_deleted = 0 and cl_to like "%_articles" and page_namespace not in (0, 2, 6, 8, 10, 12, 14, 100, 108, 118) order by rc_timestamp desc;', wikitime, thirtyminutesago)
+    query = wptools.query('wiki', 'select distinct recentchanges.rc_title, recentchanges.rc_comment, recentchances.rc_timestamp, page.page_namespace from recentchanges join page on recentchanges.rc_namespace = page.page_namespace and recentchanges.rc_title = page.page_title join categorylinks on page.page_id=categorylinks.cl_from where rc_timestamp >= %s and rc_timestamp < %s and rc_comment like "% new section" and rc_deleted = 0 and cl_to like "%_articles" and page_namespace not in (0, 2, 6, 8, 10, 12, 14, 100, 108, 118) order by rc_timestamp desc;', (wikitime, thirtyminutesago))
     
     rows = []
     for row in query:
