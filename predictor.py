a635fb9a73744f66159124bf48224a51c1582d57
harej <jamesmhare@gmail.com>
2015-06-14 18:45:41 -0400

    Experiment

--- predictor.py
+++ predictor.py
@@ -195,8 +195,8 @@ class PriorityPredictor:
         self.mostinternal = internalclout[0][1]
 
         # Weights assigned to different factors. They need to add up to 1.0.
-        self.weight_internalclout = 0
-        self.weight_pageviews = 1.0
+        self.weight_internalclout = 0.25
+        self.weight_pageviews = 0.75
         self.weight_linkcount = 0
 
         for pair in pageviews:
