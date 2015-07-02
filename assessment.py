7e69e7b38bb3596867a3ad23f1651beb6a8ab306
harej <jamesmhare@gmail.com>
2015-07-01 22:13:29 -0400

    Shorter lines

--- assessment.py
+++ assessment.py
@@ -24,22 +24,23 @@ class WikiProjectAssess:
         self.config = self.wptools.query('index', 'select json from config;', None)
         self.config = json.loads(self.config[0][0])
 
-        for project in self.config['projects']:
-            if 'assessment_tools' in project \
-            and 'at_category' in project \
-            and 'at_unknown_quality' in project \
-            and 'at_unknown_priority' in project:
-                projectname = project['name'][10:]  # Normalizing title
-                self.projects.append(projectname)
-                self.predictorseed[projectname] = project['at_category'].replace(' ', '_')
-                self.unknownquality[projectname] = project['at_unknown_quality'].replace(' ', '_')
-                self.unknownpriority[projectname] = project['at_unknown_priority'].replace(' ', '_')
+        for entry in self.config['projects']:
+            if 'assessment_tools' in entry \
+            and 'at_category' in entry \
+            and 'at_unknown_quality' in entry \
+            and 'at_unknown_priority' in entry:
+                project = entry['name'][10:]  # Normalizing title
+                self.projects.append(project)
+                self.predictorseed[project] = entry['at_category'].replace(' ', '_')
+                self.unknownquality[project] = entry['at_unknown_quality'].replace(' ', '_')
+                self.unknownpriority[project] = entry['at_unknown_priority'].replace(' ', '_')
 
 
     def qualitypredictor(self, pagetitles):
         '''
         Makes a query to ORES that predicts the quality of an article.
-        Takes list *pagetitles* as input, returns a list of tuples (title, prediction)
+        Takes list *pagetitles* as input
+        Returns a list of tuples (title, prediction)
         Input MUST be a list. If only one title, enter it as [title]
         '''
 
@@ -50,11 +51,16 @@ class WikiProjectAssess:
 
         for package in packages:
             if len(package) > 1:
-                q = 'select page_title, page_latest from page where page_namespace = 0 and page_title in {0} order by page_title limit 100;'.format(tuple(package))
+                q = ('select page_title, page_latest from page '
+                     'where page_namespace = 0 and page_title in {0} '
+                     'order by page_title limit 100;').format(tuple(package))
             else:
-                q = 'select page_title, page_latest from page where page_namespace = 0 and page_title = "{0}";'.format(package[0])
+                q = ('select page_title, page_latest from page '
+                     'where page_namespace = 0 '
+                     'and page_title = "{0}";').format(package[0])
 
-            revision_ids = {str(row[1]):row[0].decode('utf-8') for row in self.wptools.query('wiki', q, None)}
+            revision_ids = {str(row[1]):row[0].decode('utf-8') \
+                           for row in self.wptools.query('wiki', q, None)}
             api_input = [rev_id for rev_id in revision_ids.keys()]
 
             api_url = "http://ores.wmflabs.org/scores/enwiki/wp10/?revids="
@@ -73,9 +79,13 @@ class WikiProjectAssess:
 
     def qualitylist(self):
         for wikiproject, category in self.unknownquality.items():
-            save_to = "User:Reports bot/" + wikiproject + "/Assessment/Assess for quality"
-            q = 'select page_title from categorylinks join page on cl_from = page_id where cl_to = "{0}";'.format(category.replace(' ', '_'))
-            to_process = [row[0].decode('utf-8') for row in self.wptools.query('wiki', q, None)]
+            save_to = "User:Reports bot/" + wikiproject + \
+                      "/Assessment/Assess for quality"
+            q = ('select page_title from categorylinks '
+                 'join page on cl_from = page_id '
+                 'where cl_to = "{0}";').format(category.replace(' ', '_'))
+            to_process = [row[0].decode('utf-8') \
+                         for row in self.wptools.query('wiki', q, None)]
             to_process = self.qualitypredictor(to_process)
             contents = ("====Assess for quality====\n"
                         "Determine the quality of each article, then go to the "
@@ -90,7 +100,8 @@ class WikiProjectAssess:
                 contents += "<b>[[" + article + "]]</b> ([[Talk:" + article + \
                             "|talk]])<br />Predicted class: " + \
                             prediction + "|"
-            contents = contents[:-1] + "}}<includeonly>\n\n[[" + save_to + "|View more]]</includeonly>"
+            contents = contents[:-1] + "}}<includeonly>\n\n[[" \
+                       + save_to + "|View more]]</includeonly>"
 
             page = pywikibot.Page(self.bot, save_to)
             page.text = contents
@@ -122,15 +133,19 @@ class WikiProjectAssess:
                     category_recs.append(page)
 
             # Filter against these lists:
-            q = 'select pi_page from projectindex where pi_project = "Wikipedia:{0}";'
+            q = ('select pi_page from projectindex '
+                 'where pi_project = "Wikipedia:{0}";')
             q = q.format(wikiproject.replace(' ', '_'))
-            article_filter = [row[0].replace('Talk:', '') for row in self.wptools.query('index', q, None) if row[0].startswith('Talk')]
+            article_filter = [row[0].replace('Talk:', '') \
+                             for row in self.wptools.query('index', q, None) \
+                             if row[0].startswith('Talk')]
 
             q = ('select page_title from page '
                  'join categorylinks on cl_from = page_id '
                  'where page_namespace = 15 '
                  'and cl_to = "{0}";').format(category)
-            category_filter = [row[0].decode('utf-8') for row in self.wptools.query('wiki', q, None)]
+            category_filter = [row[0].decode('utf-8') \
+                              for row in self.wptools.query('wiki', q, None)]
 
             # Now do the filtering...
             category_recs = list(set(category_recs) - set(category_filter))
@@ -144,8 +159,10 @@ class WikiProjectAssess:
             recommendations = recommendations[:100]
 
             # Class prediction
-            predicted_class = self.qualitypredictor([page for page in recommendations if page.startswith(':Category:') == False]) + \
-                              [(page, 'Category') for page in recommendations if page.startswith(':Category:') == True]
+            predicted_class = self.qualitypredictor([page for page in recommendations \
+                              if page.startswith(':Category:') == False]) + \
+                              [(page, 'Category') for page in recommendations \
+                              if page.startswith(':Category:') == True]
             predicted_class = {pair[0]:pair[1] for pair in predicted_class}
 
             save_to = "User:Reports bot/" + wikiproject + "/Assessment/Not tagged"
@@ -162,7 +179,8 @@ class WikiProjectAssess:
                             + "|talk]])<br />Predicted class: " \
                             + predicted_class[recommendation] + "|"
             contents = contents.replace("Talk::Category:", "Category talk:")
-            contents = contents[:-1] + "}}<includeonly>\n\n[[" + save_to + "|View more]]</includeonly>"
+            contents = contents[:-1] + "}}<includeonly>\n\n[[" + save_to \
+                       + "|View more]]</includeonly>"
             page = pywikibot.Page(self.bot, save_to)
             page.text = contents
             page.save("Updating listing", minor=False, async=True)
