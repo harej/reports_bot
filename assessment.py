95738087d24a1a7a0beaa2b4ccb0fffd0d70c6e2
harej <jamesmhare@gmail.com>
2015-07-06 17:33:43 -0400

    Constrained width

--- assessment.py
+++ assessment.py
@@ -87,7 +87,7 @@ class WikiProjectAssess:
             to_process = [row[0].decode('utf-8') \
                          for row in self.wptools.query('wiki', q, None)]
             to_process = self.qualitypredictor(to_process)
-            contents = ("{{WPX list start|color={{{2|#086}}}|title=Assess for quality"
+            contents = ("{{WPX list start|constrained=yes|color={{{2|#086}}}|title=Assess for quality"
                         "|intro=Determine the quality of these articles<br />"
                         "{{WPX last updated|" + save_to + "}}}}<br />\n\n"
                         "{{#invoke:<includeonly>random|list|limit=5"
@@ -164,7 +164,7 @@ class WikiProjectAssess:
             predicted_class = {pair[0]:pair[1] for pair in predicted_class}
 
             save_to = "User:Reports bot/" + wikiproject + "/Assessment/Not tagged"
-            contents = ("{{WPX list start|color={{{2|#086}}}|title=Not tagged by the WikiProject|"
+            contents = ("{{WPX list start|constrained=yes|color={{{2|#086}}}|title=Not tagged by the WikiProject|"
                         "intro=These pages are potentially in the WikiProject's"
                         " scope.<br />{{WPX last updated|" + save_to + "}}}}"
                         "<br />\n\n"
