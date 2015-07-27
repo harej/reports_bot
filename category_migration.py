d23dae59690773350f015ee875cef94f866659a7
harej <jamesmhare@gmail.com>
2015-07-27 18:10:58 -0400

    New script
    This script facilitates the migration of “meta-categories” from
    WikiProject pages to their eponymous categories. This allows for
    consistent categorization of WikiProjects and streamlines the
    WikiProject directory tree.

---
+++ category_migration.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Migrates WikiProject categories from the WikiProject page to its category.
+Copyright (C) 2015 James Hare
+Licensed under MIT License: http://mitlicense.org
+"""
+
+
+import pywikibot as pwb
+from project_index import WikiProjectTools
+
+
+def main():
+    wptools = WikiProjectTools()
+    bot = pwb.Site('en', 'wikipedia', user='Harej bot')
+
+    # Generate list of WikiProjects with eponymous categories
+    q = ('select page_title from page where page_namespace = 14 '
+         'and page_title in (select page_title from page where '
+         'page_namespace = 4 and page_title like "WikiProject_%" '
+         'and page_is_redirect = 0);')
+
+    pairs = [row[0].decode('utf-8') for row in wptools.query('wiki', q, None)]
+
+    for pair in pairs:
+        # Load WikiProject page+category pairs
+        project_page = pwb.Page(bot, 'Wikipedia:' + pair)
+        project_cat = pwb.Page(bot, 'Category:' + pair)
+
+        # Preserve only categories that aren't in the style "X WikiProjects"
+        preserve  = [c for c in pwb.textlib.getCategoryLinks(project_page.text) \
+                     if str(c)[-15:] != ' WikiProjects]]']
+        project_page.text = pywikibot.textlib.replaceCategoryLinks(project_page.text, preserve)
+
+        # List categories to add to project category
+        page_cats = [c for c in pwb.textlib.getCategoryLinks(project_page.text) \
+                     if str(c)[-15:] == ' WikiProjects]]']
+        cat_cats  = [c for c in pwb.textlib.getCategoryLinks(project_cat.text) \
+                     if str(c)[-15:] == ' WikiProjects]]']
+
+        to_add = list(set(page_cats) - set(cat_cats))
+        project_cat.text = pywikibot.textlib.replaceCategoryLinks(project_cat.text, to_add, addOnly=True)
+
+        # Saving pages
+        summary = "WikiProject category migration. See [[User:Harej bot/WikiProject category migration]]."
+        project_page.save(summary, minor=False)
+        project_cat.save(summary, minor=False)
+
+
+if __name__ == "__main__":
+    main()
