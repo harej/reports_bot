a9940ef10215ce8e18fc318d2c283cf83aa3b5b2
harej <jamesmhare@gmail.com>
2015-08-19 11:54:37 -0400

    Potential bug fix?

--- no_wikidata.py
+++ no_wikidata.py
@@ -14,15 +14,14 @@ def main():
     wptools = WikiProjectTools()
     bot = pywikibot.Site('en', 'wikipedia')
 
-    q = ('select page_title from page where page_namespace = 0 '
-         'and page_is_redirect = 0 order by page_id;')
-    all_articles = [x[0].decode('utf-8') for x in wptools.query('wiki', q, None)]
+    q1 = ('select page_title from page where page_namespace = 0 '
+          'and page_is_redirect = 0 order by page_id;')
+    all_articles = [x[0].decode('utf-8') for x in wptools.query('wiki', q1, None)]
 
-    q = ('select page_title from page join page_props on pp_page = page_id '
-         'where page_namespace = 0 and pp_propname = "wikibase_item '
-         'order by page_id";')
-    articles_on_wikidata = [x[0].decode('utf-8') \
-                            for x in wptools.query('wiki', q, None)]
+    q2 = ('select page_title from page join page_props on pp_page = page_id '
+          'where page_namespace = 0 and pp_propname = "wikibase_item '
+          'order by page_id";')
+    articles_on_wikidata = [x[0].decode('utf-8') for x in wptools.query('wiki', q2, None)]
 
     no_wikidata = []
     for title in all_articles:
