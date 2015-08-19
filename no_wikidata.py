af905da33b2ac0993cb75919fb823fa15a05de19
harej <jamesmhare@gmail.com>
2015-08-19 02:01:30 -0400

    Making output oldest to newest
    This way, extremely new articles that may end up getting deleted don’t
    end up in the list

--- no_wikidata.py
+++ no_wikidata.py
@@ -14,15 +14,21 @@ def main():
     wptools = WikiProjectTools()
     bot = pywikibot.Site('en', 'wikipedia')
 
-    q = 'select page_title from page where page_namespace = 0 and page_is_redirect = 0;'
+    q = ('select page_title from page where page_namespace = 0 '
+         'and page_is_redirect = 0 order by page_id;'
     all_articles = [x[0].decode('utf-8') for x in wptools.query('wiki', q, None)]
 
     q = ('select page_title from page join page_props on pp_page = page_id '
-         'where page_namespace = 0 and pp_propname = "wikibase_item";')
+         'where page_namespace = 0 and pp_propname = "wikibase_item '
+         'order by page_id";')
     articles_on_wikidata = [x[0].decode('utf-8') \
                             for x in wptools.query('wiki', q, None)]
 
-    no_wikidata = list(set(all_articles) - set(articles_on_wikidata))
+    no_wikidata = []
+    for title in all_articles:
+        if title not in articles_on_wikidata:
+            no_wikidata.append(title)
+
     total_count = len(no_wikidata)  # Capturing this before truncating list
     no_wikidata = no_wikidata[:100]
 
