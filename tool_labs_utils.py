6714c626b9c2d1b7717490ec858f0c63cf588dae
harej <jamesmhare@gmail.com>
2015-11-14 22:03:24 -0500

    New script tool_labs_utils.py

---
+++ tool_labs_utils.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Generic utility for querying a database on Tool Labs
+Copyright (C) 2015 James Hare
+Licensed under MIT License: http://mitlicense.org
+"""
+
+class ToolLabsQuery:
+    def raw_query(self, host, db, sqlquery, values):
+        """Generic wrapper for carrying out MySQL queries"""
+
+        conn = pymysql.connect(host=host, port=3306, db=db, read_default_file='~/.my.cnf', charset='utf8')
+        cur = conn.cursor()
+        cur.execute(sqlquery, values)
+        data = []
+        for row in cur:
+            data.append(row)
+        conn.commit()
+        return data
+
+    def wmf(self, db, sqlquery, values):
+        """Queries for WMF wiki database replicas on Labs (e.g. enwiki)"""
+        return self.raw_query(db + '.labsdb', db + '_p', sqlquery, values)
+
+    def toolsdb(self, db, sqlquery, values):
+        """Queries a Tool Labs database"""
+        return self.raw_query('tools-db', db, sqlquery, values)
